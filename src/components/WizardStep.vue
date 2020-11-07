9c1e5f55a0acc259d6011674edad39cca6a45dde
Juan C Robles J <project.ufa@gmail.com>
2020-11-07 12:42:27 -0600

    add support for icon pack

--- src/components/WizardStep.vue
+++ src/components/WizardStep.vue
@@ -18,14 +18,14 @@
                  :style="[tab.active ? iconActiveStyle: {}, tab.validationError ? errorStyle : {}]">
               <slot name="active-step">
                 <i v-if="tab.icon" :class="[tab.iconPack, tab.iconPack !== 'material-icons' ? tab.icon : '']" class="wizard-icon">
-                  {{ tab.iconPack === 'material-icons' ? icon : '' }}
+                  {{ tab.iconPack === 'material-icons' ? tab.icon : '' }}
                 </i>
                 <i v-else class="wizard-icon">{{index + 1}}</i>
               </slot>
             </div>
             <slot v-if="!tab.active">
               <i v-if="!tab.active && tab.icon" :class="[tab.iconPack, tab.iconPack !== 'material-icons' ? tab.icon : '']" class="wizard-icon">
-                {{ tab.iconPack === 'material-icons' ? icon : '' }}
+                {{ tab.iconPack === 'material-icons' ? tab.icon : '' }}
               </i>
               <i v-if="!tab.active && !tab.icon" class="wizard-icon">{{index + 1}}</i>
             </slot>
